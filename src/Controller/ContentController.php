d7fabe801ff907d1122c8d9a0e89ac94788d0073
Alexander <fischer.online@gmx.net>
2022-06-13 12:29:17 +0200

    Creating ContentController portalAction and passing it to the Portal Template

---
+++ src/Controller/ContentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Pimcore\Controller\FrontendController;
+
+class ContentController extends FrontendController
+{
+    public function portalAction()
+    {
+        return $this->render('content/portal.html.twig', [
+            'isPortal' => true
+        ]);
+    }
+}
