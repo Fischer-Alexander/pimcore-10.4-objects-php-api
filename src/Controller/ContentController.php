e5ca71e6f9197c44e2456005ea9cee5b8ecf0c8d
Alexander <fischer.online@gmx.net>
2022-06-14 20:40:32 +0200

    Creating the Template for a Static Site and Preparing Initial Setup of Area Bricks

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -5,9 +5,19 @@ namespace App\Controller;
 use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Pimcore\Model\DataObject\Blog;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 class ContentController extends FrontendController
 {
+    /**
+     * @Template
+     */
+    public function defaultAction()
+    {
+        return[];
+    }
+
     public function portalAction()
     {
         $blogList = new Blog\Listing();
