bdf59f19d8ad75518e2cf94f760006aea58e3628
Alexander <fischer.online@gmx.net>
2022-06-15 16:13:46 +0200

    Adding the Search Functionality

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -6,6 +6,7 @@ use Pimcore\Controller\FrontendController;
 use Symfony\Component\HttpFoundation\Request;
 use Pimcore\Model\DataObject\Blog;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 
 class ContentController extends FrontendController
@@ -17,6 +18,41 @@ class ContentController extends FrontendController
     {
         return[];
     }
+
+
+    /**
+     * @Route("/", name="app_search", methods="POST")
+     */
+    public function search(Request $request)
+    {
+        $searchterm = $request->request->get('searchterm');
+
+        $search_hits= [];
+
+        $blogList = new Blog\Listing();
+        $blogList->setOrderKey('date');
+        $blogList->setOrder('DESC');
+
+        foreach ($blogList as $item) {
+
+            $blogtitel = $item->getTitle();
+            $blogcontent = $item->getContent();
+
+            if (str_contains($blogtitel, $searchterm) || str_contains($blogcontent, $searchterm)) {
+                array_push($search_hits, $item);
+            }
+        }
+
+            return $this->render('blog/listing.html.twig', [
+                'isPortal' => true,
+                'blogs' => $search_hits,
+            ]);
+
+        }
+
+
+
+
 
     public function portalAction()
     {
