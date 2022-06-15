596d7194ac7040961ad8d8b55224291bd9b98740
Alexander <fischer.online@gmx.net>
2022-06-15 07:55:54 +0200

    Implementing an Area Brick Renderlet for a Blog Object and Creating the Renderlet Template

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,6 +8,7 @@ use Pimcore\Model\DataObject\Blog;
 use Pimcore\Model\DataObject\Concrete;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends FrontendController
@@ -22,5 +23,24 @@ class BlogController extends FrontendController
         return $this->render('blog/detail.html.twig', [
            'blog' => $blog,
         ]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function blogTeaserAction(Request $request)
+    {
+
+        if ($request->get('type') == 'object') {
+            $blog = Blog::getById($request->get('id'));
+
+            return $this->render('blog/blog_teaser.html.twig', [
+                'blog' => $blog,
+            ]);
+        }
+
+        throw new NotFoundHttpException('Blog not found.');
     }
 }
